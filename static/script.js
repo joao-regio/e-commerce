2664bafe96853cdf11c3ced7ab22ff4312faf29b
João Vitor <jv2005.jvar@gmail.com>
2022-03-10 21:20:19 -0300

    primeiros passos filtrando itens por categorias

--- static/script.js
+++ static/script.js
@@ -10,7 +10,7 @@ function openMenuMobile(event){
 	}
 
 	//ao clicar no botão abrir menu mobile
-	nav.classList.toggle('active')
+	nav.classList.toggle('active');
 
 	//quando nav tiver a classe active mudar img para o botão X
 	if(nav.classList.contains('active')){
@@ -21,5 +21,85 @@ function openMenuMobile(event){
 
 }
 
-menuMobile.addEventListener('click', openMenuMobile)
-menuMobile.addEventListener('touchstart', openMenuMobile)
+menuMobile.addEventListener('click', openMenuMobile);
+menuMobile.addEventListener('touchstart', openMenuMobile);
+
+
+//quando o usuário clicar nos botões relevantes as categorias de roupas da loja filtrar por categorias
+//então quando clicar em camisa aparecer só camisas, quando clicar em moletom aparecer só moletom e assim por diante
+
+//camisa
+const shirt = document.querySelectorAll('.itens-loja .camisa');
+const btnShirt = document.getElementById('selecionar-camisa');
+
+//moletom
+const sweatshirt = document.querySelectorAll('.itens-loja .moletom');
+const btnSweatshirt = document.getElementById('selecionar-moletom');
+
+//calça
+const pants = document.querySelectorAll('.itens-loja .calca');
+const btnPants = document.getElementById('selecionar-calca');
+
+
+function getShirt(){
+	//pegar camisas e selecionar os itens com a classe camisa na loja
+	sweatshirt.forEach((item, index)=>{
+		//retirar moletons
+		sweatshirt[index].style.display = 'none';
+	})
+
+	pants.forEach((item, index)=>{
+		//retirar calças
+		pants[index].style.display = 'none';
+	})
+
+	if(shirt.style.display === 'none'){
+		shirt.forEach((item, index)=>{
+			shirt[index].style.display = 'flex';
+		})
+	}
+
+}
+
+function getSweatshirt() {
+	//pegar moletons e selecionar os itens com a classe moletom na loja
+	shirt.forEach((item,index)=>{
+		//remover camisas
+		shirt[index].style.display = 'none'
+	})
+
+	pants.forEach((item, index)=>{
+		//retirar calças
+		pants[index].style.display = 'none';
+	})
+
+	if(sweatshirt.style.display === 'none'){
+		sweatshirt.forEach((item, index)=>{
+			sweatshirt[index].style.display = 'flex';
+		})
+	}
+}
+
+function getPants() {
+	//pegar calca e selecionar os itens com a classe calcas na loja
+	shirt.forEach((item,index)=>{
+		//remover camisas
+		shirt[index].style.display = 'none'
+	})
+
+	sweatshirt.forEach((item, index)=>{
+		//retirar moletons
+		sweatshirt[index].style.display = 'none';
+	})
+
+	if(spants.style.display === 'none'){
+		pants.forEach((item, index)=>{
+			pants[index].style.display = 'flex';
+		})
+	}
+}
+
+
+btnShirt.addEventListener('click', getShirt);
+btnSweatshirt.addEventListener('click', getSweatshirt);
+btnPants.addEventListener('click', getPants);
